f6beb998e9f1d1ac49a5077c751b33b0afd689b0
Ian <ip240z@protonmail.com>
2023-05-24 22:43:54 -0400

    added a neat logo in the center of the home page. Need to address the issues with trying to create articles when fetch fails

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import StockBody from './components/StockBody';
 import NoMatch from './components/NoMatch';
 import { Routes, Route } from 'react-router-dom';
 import bgPic from "./mainBG.png"
+import logo from "./logo.png"
 
 
 function App() {
 
 
   return (
-    <div className="App" >
-      <Routes>
-        <Route path="/*" element={<StockBody />} />
-        {/* <Route path="*" element={<NoMatch />} /> */}
-      </Routes>
-    </div>
+    <main className='App'>
+        <Routes>
+          <Route path="/*" element={<StockBody />} />
+          {/* <Route path="*" element={<NoMatch />} /> */}
+        </Routes>
+        <div className='bg'>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        <img src={logo} alt="blarg?" className='bgImg'/>
+        </div>
+     
+    </main>
   );
 }
 
